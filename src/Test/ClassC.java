bfd648bb0815fbc911ffc16b3d39ed72a19466c3
voletibhagya <voletibhagya@gmail.com>
2025-08-15 20:58:04 +0530

    Update ClassC.java

--- src/Test/ClassC.java
+++ src/Test/ClassC.java
@@ -6,7 +6,7 @@ public class ClassC {public static void main(String ar[])
 	System.out.println("ClassB changes");
 	System.out.println("C New changes");
 	System.out.println("C New sprint12");
-	
+	System.out.println("New changes to C");
 }
 
 
